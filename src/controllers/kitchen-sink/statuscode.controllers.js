c26e92eb87f72a547396de79eb306ef5bd1dc339
MrBobbydayal <bobbydayal398@gmail.com>
2025-06-02 21:46:29 +0530

    same

--- src/controllers/kitchen-sink/statuscode.controllers.js
+++ src/controllers/kitchen-sink/statuscode.controllers.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../../utils/asyncHandler.js";
-import statusCodesJson from "../../json/status-codes.json";
+const statusCodesJson = await import('../../json/status-codes.json', { assert: { type: 'json' } }).then(mod => mod.default);
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 
